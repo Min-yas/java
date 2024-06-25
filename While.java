2fbe2dec15addd0f27dd9b6f5a496005c72e4cde
Yasmina K <yasminakhatun2404@gmail.com>
2024-06-26 00:14:06 +0530

    Update While.java

--- While.java
+++ While.java
@@ -1,19 +1,20 @@
+'''while loop'''
 import java.util.Scanner;
 
 class While
 {
 	public static void main(String args[])
 	{
-		int n;
+		int num;
 
 		System.out.println("Enter value for condition: ");
 		Scanner r=new Scanner(System.in);
-		n=r.nextInt();
+		num=r.nextInt();
 		
-		while(n>=0)
+		while(num>=0)
 		{
 			System.out.println("learn coding");
-			n-=n;
+			num-=num;
 		}
 	}
 }
